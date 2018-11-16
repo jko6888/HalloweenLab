3c23ac126c5618b4c8a7d0f887035765fae20d3a
james <jko6888@bths.edu>
2018-11-15 22:04:18 -0500

    Kevin

--- JackOLantern.java
+++ JackOLantern.java
@@ -1,4 +1,32 @@
-package JamesKo;
+public class JackOLantern extends java.lang.Object{
+    String[][] faceFeatures;
 
-public class JackOLantern {
+    public JackOLantern(java.lang.String[][] faceFeatures)
+    {
+        this.faceFeatures = faceFeatures;
+    }
+    public void edit(java.lang.String replace, int row, int column){
+        faceFeatures[row][column] = replace;
+    }
+    public void fill(java.lang.String str) {
+        for (int i = 0; i < faceFeatures.length; i++) {
+            for (int j = 0; j < faceFeatures[i].length; j++) {
+                edit(str, i, j);
+            }
+        }
+    }
+    public java.lang.String toString()
+    {
+        String face = "";
+        for(int i = 0;i<faceFeatures.length;i++)
+        {
+            for (int x = 0; x < faceFeatures.length; x++)
+            {
+                face += faceFeatures[i][x];
+            }
+            face += "\n";
+        }
+        return face;
+    }
+
 }
---
+++ Runner.java
@@ -0,0 +1,18 @@
+public class Runner {
+    public static void main(String[] args)
+    {
+        String[][] mySpookyLantern = new String[8][8];
+        JackOLantern jackOLantern = new JackOLantern(mySpookyLantern);
+        jackOLantern.fill("*");
+        jackOLantern.edit("A", 2, 1);
+        jackOLantern.edit("A", 2, 6);
+        jackOLantern.edit("0", 5, 1);
+        for(int i = 0;i<4;i++)
+        {
+            jackOLantern.edit("0", 6, 2 + i );
+        }
+        jackOLantern.edit("0", 5, 6);
+        System.out.print(jackOLantern.toString());
+
+    }
+}
